b8a79c98a84942c710316270001457283485ae60
John Lord <john@lordsonline.org>
2020-03-21 17:26:29 +0100

    Added documentation

--- R/ls_test.R
+++ R/ls_test.R
@@ -1,3 +1,34 @@
+#' ls_test
+#'
+#' Helper function that returns parameter estimates, effect sizes, pair-wise
+#' comparisons and power t-test calculations.
+#'
+#' @param model
+#' a model
+#' @param contrast
+#' variable used for least square mean estimates and contrasts.
+#' @param control
+#' factor level used as the control value for contrasts
+#' @param sig.level
+#' signficance level used in power calculations
+#' @param power
+#' power used in power calculations
+#'
+#'
+#'
+#' @return
+#' A list consisting of the following:
+#' \describe{
+#' \item{estimates}{summary(model)}
+#' \item{ls_means}{least square means table (emmGrid object) using proportional
+#' weighting}
+#' \item{contrasts}{pair-wise contrast table (emmGrid object) against control
+#' using no multiplicity correction}
+#' \item{power}{power.t.test result}
+#' }
+#' @export
+#'
+#' @examples
 ls_test <-
   function(model, contrast, control, sig.level = .05, power = .9) {
     sigma <- summary(model)$sigma
