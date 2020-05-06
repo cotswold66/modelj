d813350e61497012c47e80274f316be97c484c95
John Lord <john@lordsonline.org>
2020-05-06 07:43:59 +0200

    Added caption

--- R/summary.R
+++ R/summary.R
@@ -1,15 +1,28 @@
 #' Method for printing summary table
 #'
-#' @param ...
 #' @param object modelj object
+#' @param caption string containing table caption
 #'
 #' @return
 #' @export
 #'
-summary.modelj <- function(object, ...){
+summary.modelj <- function(object, caption = NULL){
   x <- purrr::pluck(object, "summary")
-  x <- knitr::kable(x, digits = c(0, 2, 2, 2, 2, 0, 4, 4 ,2, 0 ,2, 0))
-  x <- kableExtra::kable_styling(x)
-  x <- kableExtra::add_header_above(x, c(" " = 1, "Contrasts Statistics" = 7, "Power Calc" = 4))
-  x
+  x <- knitr::kable(
+    x,
+    booktabs = TRUE,
+    linesep = "",
+    caption = caption,
+    digits = c(0, 2, 2, 2, 2, 0, 4, 4 ,2, 0 ,2, 0)
+    )
+  x <- kableExtra::kable_styling(
+    x,
+    latex_options = c("striped", "HOLD_posiiton"),
+    full_width = FALSE
+    )
+  x <- kableExtra::add_header_above(
+    x,
+    c(" " = 1, "Contrasts Statistics" = 7, "Power Calc" = 4)
+    )
+  return(x)
 }
